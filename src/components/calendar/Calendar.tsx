2486b04a3d03f6b9d1244fc8610d5a5b4ea1ff98
emilschultz <emil.schultz@amedia.no>
2024-01-23 22:41:31 +0100

    feat: calendar component and functionality

---
+++ src/components/calendar/Calendar.tsx
@@ -0,0 +1,55 @@
+import { DateRange } from 'react-date-range';
+import { useState } from 'react';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import styles from './calendar.module.css';
+
+interface NewRange {
+  startDate: Date;
+  endDate: Date;
+  key: string;
+  color: string;
+}
+
+export default function Calendar() {
+  const [range, setRange] = useState<NewRange[]>([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection',
+      color: '#d04e4a',
+    },
+  ]);
+
+  const handleDateChange = (dateItems: { selection?: NewRange }) => {
+    if (
+      dateItems.selection &&
+      dateItems.selection.startDate &&
+      dateItems.selection.endDate
+    ) {
+      setRange([dateItems.selection]);
+      console.log(range[0]);
+    }
+  };
+
+  const lastDateInRange = range.length - 1;
+  const lastEndDate = range[lastDateInRange].endDate.toISOString();
+
+  return (
+    <>
+      <div className={styles.calendar}>
+        <div>
+          <DateRange
+            className={styles.wrap}
+            onChange={handleDateChange}
+            moveRangeOnFirstSelection={false}
+            ranges={range}
+            months={1}
+            direction='horizontal'
+            color='#fff'
+          />
+        </div>
+      </div>
+    </>
+  );
+}
