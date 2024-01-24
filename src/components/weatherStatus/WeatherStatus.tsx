9d64090c69a44e974abbd2af747f1e59d2bf8158
emilschultz <emil.schultz@amedia.no>
2024-01-24 09:46:35 +0100

    update and seperate functions for weather

--- src/components/weatherStatus/WeatherStatus.tsx
+++ src/components/weatherStatus/WeatherStatus.tsx
@@ -10,45 +10,45 @@ interface WeatherStatusProps {
   dateRange: dateRangeLength;
 }
 
+const getWeatherForRange = (startDate: Date, endDate: Date) => {
+  const matchingWeatherEntry = mockData.find((entry) => {
+    const currentDate = new Date(entry.date);
+    return currentDate >= startDate && currentDate <= endDate;
+  });
+
+  return matchingWeatherEntry?.weather || 'unknown';
+};
+
+const getWeatherIcon = (weather: string) => {
+  switch (weather) {
+    case 'sunny':
+      return '☀️';
+    case 'cloudy':
+      return '☁️';
+    case 'snow':
+      return '❄️';
+    case 'clear':
+      return '✨';
+    case 'partly cloudy':
+      return '⛅';
+    case 'windy':
+      return '🌬️';
+    default:
+      return '🤷';
+  }
+};
+
 export default function WeatherStatus({ dateRange }: WeatherStatusProps) {
-  const getWeatherForRange = () => {
-    const startDate = new Date(dateRange.startDate);
-    const endDate = new Date(dateRange.endDate);
-
-    const matchingWeatherEntry = mockData.find((entry) => {
-      const currentDate = new Date(entry.date);
-      return currentDate >= startDate && currentDate <= endDate;
-    });
-
-    return matchingWeatherEntry?.weather || 'unknown';
-  };
-
-  const weatherStatus = () => {
-    const weather = getWeatherForRange();
-
-    switch (weather) {
-      case 'sunny':
-        return '☀️';
-      case 'cloudy':
-        return '☁️';
-      case 'snow':
-        return '❄️';
-      case 'clear':
-        return '✨';
-      case 'partly cloudy':
-        return '⛅';
-      case 'windy':
-        return '🌬️';
-      default:
-        return '🤷';
-    }
-  };
+  const startDate = new Date(dateRange.startDate);
+  const endDate = new Date(dateRange.endDate);
+  const weather = getWeatherForRange(startDate, endDate);
+  const weatherIcon = getWeatherIcon(weather);
 
   return (
     <>
       <h1 className={styles.message}>
-        The weather in this period will most likely be: {weatherStatus()}{' '}
-        {getWeatherForRange()} {weatherStatus()}
+        The weather in this period will most likely be: {weatherIcon} {weather}{' '}
+        {weatherIcon}
       </h1>
     </>
   );
